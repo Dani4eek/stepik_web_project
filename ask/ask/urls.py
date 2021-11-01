13becc31aacd1a5de0c6e04c572d25b592f3e678
Dani4eek <71774139+Dani4eek@users.noreply.github.com>
2021-11-01 21:11:30 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from qa.views import test
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^$', include('qa.urls')),
     url(r'^login/$', include('qa.urls')),
     url(r'^signup/$', include('qa.urls')),
-    url(r'^question/(\d+)/$', include('qa.urls')),
+    url(r'^question/\d+/$', include('qa.urls')),
     url(r'^ask/$', include('qa.urls')),
     url(r'^popular/$', include('qa.urls')),
     url(r'^new/$', include('qa.urls'))
