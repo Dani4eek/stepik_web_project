16311e2019f71998b426a145f3263bac8d3cafd1
Dani4eek <71774139+Dani4eek@users.noreply.github.com>
2021-11-02 14:09:26 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,6 +8,10 @@ sudo /etc/init.d/nginx restart
 #sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application
 #sudo /etc/init.d/gunicorn restart
 
+sudo pip3 install django==1.10.3 gunicorn==19.6.0
+sudo sed -i -e "s@/usr/bin/python@/usr/bin/python3@" /usr/sbin/gunicorn-debian /usr/bin/gunicorn /usr/bin/gunicorn_django /usr/bin/gunicorn_paster
+sudo sed -i -e "s@gunicorn==17.5@gunicorn==19.6.0@g" /usr/bin/gunicorn /usr/bin/gunicorn_django /usr/bin/gunicorn_paster
+
 sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/test_django
 sudo /etc/init.d/gunicorn restart
