4c93e3bf0d010ee3e96e04a8976ff1a43daca715
Dani4eek <71774139+Dani4eek@users.noreply.github.com>
2021-11-02 13:28:24 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/gunicorn_django.conf
+#sudo ln -sf /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/gunicorn_django.conf
 gunicorn -c /etc/gunicorn.d/gunicorn_django.conf ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
