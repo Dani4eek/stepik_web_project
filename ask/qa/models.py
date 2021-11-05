97f70a2889a672186f5be2575741db555b69c6b9
Dani4eek <71774139+Dani4eek@users.noreply.github.com>
2021-11-05 23:36:47 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Question(models.Model)
+class Question(models.Model):
+  title = models.CharField(max_length = 255)
+  text = models.TextField()
+  added_at = models.DateTimeField(blank = True)
+  rating = 
+  author = 
+  likes = 
