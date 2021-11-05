b2964832528b504db56f482d457ee614367b76d8
Dani4eek <71774139+Dani4eek@users.noreply.github.com>
2021-11-05 23:14:47 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+sudo /etc/init.d/mysql start
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
