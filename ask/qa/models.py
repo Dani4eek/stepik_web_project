1b97bfe5d6ff42a0243e21cf9a5320d816382338
Dani4eek <71774139+Dani4eek@users.noreply.github.com>
2021-11-06 11:32:29 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,12 +1,34 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
-  title = models.CharField(max_length = 255)
-  text = models.TextField()
-  added_at = models.DateTimeField(blank = True)
-  rating = 
-  author = 
-  likes = 
+    objects = QuestionManager()
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id)
