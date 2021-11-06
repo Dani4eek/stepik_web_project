3441df4c8da1e72183b9bc219df222d9c1d4f2c5
Dani4eek <71774139+Dani4eek@users.noreply.github.com>
2021-11-06 11:33:54 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,12 @@
-sudo /etc/init.d/mysql start
-mysql -uroot -e "create database stepik_web;"
-mysql -uroot -e "grant all privileges on stepik_web.* to 'box'@'localhost' with grant option;"
-~/web/ask/manage.py makemigrations
-~/web/ask/manage.py migrate
-
-
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/gunicorn_django.py
 sudo /etc/init.d/gunicorn restart
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepik_web;"
+mysql -uroot -e "grant all privileges on stepik_web.* to 'box'@'localhost' with grant option;"
+~/web/ask/manage.py makemigrations
+~/web/ask/manage.py migrate
